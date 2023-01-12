0fe429cf31f867dfa417e8d616045bbaed402eff
ryanvann33 <rvann377@gmail.com>
2023-01-12 14:01:46 -0500

    dadawdw

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,12 +1,25 @@
-function greet(name) {
-  const greetPromise = new Promise(function (resolve, reject) {
-    resolve(`Hello ${name}`);
-  });
-  return greetPromise;
-}
-const suzie = greet("suzie");
-console.log(suzie);
+// function greet(name) {
+//   const greetPromise = new Promise(function (resolve, reject) {
+//     resolve(`Hello ${name}`);
+//   });
+//   return greetPromise;
+// }
+// const suzie = greet("suzie");
+// console.log(suzie);
 
-suzie.then((result) => {
-  console.log(result);
-});
+// suzie.then((result) => {
+//   console.log(result);
+// });
+
+const URL = "https://api.quotable.io/random";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json(); //makes the data into a json we can use
+    document.getElementById("api-response").textContent = data.content;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
